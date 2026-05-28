d2047c93ba22fa444413edfc6bab7c4755b7e993
yunfeiyang-git <lbnshj@qq.com>
2026-05-28 10:50:51 +0800

    fix: enhance WebView settings for better JavaScript support

--- android/app/src/main/java/com/cookbook/app/MainActivity.java
+++ android/app/src/main/java/com/cookbook/app/MainActivity.java
@@ -2,6 +2,8 @@ package com.cookbook.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -17,12 +19,25 @@ public class MainActivity extends AppCompatActivity {
 
         webView = findViewById(R.id.webview);
         WebSettings webSettings = webView.getSettings();
+        
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true);
         webSettings.setAllowFileAccess(true);
         webSettings.setAllowContentAccess(true);
+        webSettings.setAllowFileAccessFromFileURLs(true);
+        webSettings.setAllowUniversalAccessFromFileURLs(true);
+        webSettings.setMediaPlaybackRequiresUserGesture(false);
 
-        webView.setWebViewClient(new WebViewClient());
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(request.getUrl().toString());
+                return true;
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient());
+        
         webView.loadUrl("file:///android_asset/public/index.html");
     }
 
