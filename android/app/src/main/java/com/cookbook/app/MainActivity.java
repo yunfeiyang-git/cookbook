39875e948451817d238a0079863a9b993216ce80
yunfeiyang-git <lbnshj@qq.com>
2026-05-28 10:07:37 +0800

    fix: update MainActivity to use AppCompatActivity instead of BridgeActivity

--- android/app/src/main/java/com/cookbook/app/MainActivity.java
+++ android/app/src/main/java/com/cookbook/app/MainActivity.java
@@ -1,5 +1,12 @@
 package com.cookbook.app;
 
-import com.getcapacitor.BridgeActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
